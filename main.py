bd02ad30b158803464fe2579426a11ed27bd301b
Maxim <101943885+PlumPlumm@users.noreply.github.com>
2023-01-27 11:56:16 +0500

    init read text

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ TOKEN = '5899343319:AAHKzqJLAfx2GnIxdoZ0yIbd4A0FJhNjVX8'
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
+TEXT = []
+with open("message_text.txt", 'r', encoding="utf-8") as f:
+    for line in f:
+        line = line.replace("\n", '')
+        TEXT.append(line)
 
 @dp.message_handler(commands=['start'])
 async def echo(message: types.Message):
     await message.answer(text='ппп')
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    # executor.start_polling(dp)
+    print(TEXT)

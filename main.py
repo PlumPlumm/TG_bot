313ea9d2942bd8553dadbf89ba0fb909b6e90167
Maxim <101943885+PlumPlumm@users.noreply.github.com>
2023-01-27 11:07:55 +0500

    init aiogram

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = '5899343319:AAHKzqJLAfx2GnIxdoZ0yIbd4A0FJhNjVX8'
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['start'])
+async def echo(message: types.Message):
+    await message.answer(text='ппп')
 
 if __name__ == '__main__':
-    pass
+    executor.start_polling(dp)

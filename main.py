7267952588134d4a1ea730ca13f67d69733074c4
Maxim <101943885+PlumPlumm@users.noreply.github.com>
2023-01-27 12:02:52 +0500

    add message text

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from aiogram import Bot, Dispatcher, executor, types
 
 TOKEN = '5899343319:AAHKzqJLAfx2GnIxdoZ0yIbd4A0FJhNjVX8'
 
+morning_text_path = 'text/morning_text.txt'
+night_text_path = 'text/night_text.txt'
+
+def read_text(path):
+    text = []
+    with open(path, 'r', encoding="utf-8") as f:
+        for line in f:
+            line = line.replace("\n", '')
+            text.append(line)
+    f.close()
+    return text
+
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
-
-TEXT = []
-with open("message_text.txt", 'r', encoding="utf-8") as f:
-    for line in f:
-        line = line.replace("\n", '')
-        TEXT.append(line)
 
 @dp.message_handler(commands=['start'])
 async def echo(message: types.Message):
@@ -17,4 +23,4 @@ async def echo(message: types.Message):
 
 if __name__ == '__main__':
     # executor.start_polling(dp)
-    print(TEXT)
+    print(read_text(night_text_path))
